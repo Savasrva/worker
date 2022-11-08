0852a985a8dbef1798dadfa16bec5187fde8bdf5
Savasrva <mgk7098@gmail.com>
2022-11-08 13:19:05 +0900

    bug fixed

--- index.js
+++ index.js
@@ -17,16 +17,19 @@ const requestBody = (startDate, submitDate) => {
         submitDate
     };
 };
-const options = {
-    uri: "https://forms.office.com/formapi/api/9188040d-6c67-4c5b-b112-36a304b66dad/users/00000000-0000-0000-0003-bffd8053179c/forms('DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAO__YBTF5xUMVo1VTdRNVk4RVA4SENIQllNWEdKRTBNNi4u')/responses",
-    body: requestBody(new Date(new Date().getTime() - 2000).toISOString(), new Date().toISOString()),
-    json: true
+const options = (startDate, submitDate) => {
+    return {
+        uri: "https://forms.office.com/formapi/api/9188040d-6c67-4c5b-b112-36a304b66dad/users/00000000-0000-0000-0003-bffd8053179c/forms('DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAO__YBTF5xUMVo1VTdRNVk4RVA4SENIQllNWEdKRTBNNi4u')/responses",
+        body: requestBody(startDate, submitDate),
+        json: true
+    }
 };
 
 
-schedule.scheduleJob('* 2 * * * *', function(){
-    console.log('2Min');
-    request.post(options, function (err, res, body) {
+schedule.scheduleJob('* * * * * ', function(){
+    console.log('매분 반복');
+    let opt = options(new Date(new Date().getTime() - 2000).toISOString(), new Date().toISOString());
+    request.post(opt, function (err, res, body) {
         console.log(res);
     });
 });
